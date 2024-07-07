1379abb06a70f9b009a4944bee3cb809e492d6c7
Jonathan Forson <jonathanforsondanso@gmail.com>
2024-07-07 13:06:08 +0000

    initial set up of django authentication

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
+
+for more information on allauth,see
+https://docs.allauth.org/en/latest/installation/quickstart.html
 """
 
 from pathlib import Path
@@ -26,7 +29,7 @@ SECRET_KEY = 'django-insecure-bryzqv(cobo#(c2y)l0tixbqwx-dl&d2urk*j!ivco1==^&*dj
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8000-jonfd4-boutiqueado-1fm5c19xn0x.ws-eu114.gitpod.io'
+   '8000-jonfd4-boutiqueado-1fm5c19xn0x.ws-eu115.gitpod.io'
 ]
 
 
@@ -39,6 +42,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+
+    #other apps
+    'allauth',
+    'allauth.account', # the app that allows basic user account stuff like logging in and out, user registration and password reset.
+     # Optional -- requires install using `django-allauth[socialaccount]`. It specifically handles logging in via social media accounts.
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware'
 ]
 
 ROOT_URLCONF = 'boutique_ado.urls'
@@ -61,13 +73,38 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.template.context_processors.request', #required by allauth. It allows allauth and django itself to access the HTTP request objects in our templates.
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
+
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by email
+    'allauth.account.auth_backends.AuthenticationBackend', # allows users to log in via email address
+    
+]
+
+SITE_ID = 1
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+#Tells allauth to allow authentication methods via username or emails.
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+
+#These three make it so that an email is required, verification is mandatory.
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
