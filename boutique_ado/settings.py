1007526f76a3e65eb2caa61e5881105f088e1b6d
Jonathan Forson <jonathanforsondanso@gmail.com>
2024-07-09 10:38:40 +0000

    Add static file and media settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -163,6 +163,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# typically a static_root setting will be added for django collectstatic utility, however, this interfers with setting up AWS
+
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
