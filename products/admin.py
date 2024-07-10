5b7a1026f92770255a7fe8561cc86706b0ba766f
Jonathan Forson <jonathanforsondanso@gmail.com>
2024-07-10 21:11:35 +0000

    customise admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from .models import Category, Product
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display=(
+        'sku', 'name', 'category','price', 'rating',
+        'image'
+    )
+
+    # sort the products by sku. Although it is one field, it is possible to sort on multiple columns. Hence, it is in a tuple. Reverse by add a 'minus' at the end.
+    ordering=('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=(
+        'friendly_name',
+        'name'
+    )
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
