7b3520866128cea6dcbc1717cf5ffd171de2aaa0
Jonathan Forson <jonathanforsondanso@gmail.com>
2024-07-13 16:38:10 +0000

    Add products views and templates

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static # used to add media url to urls.py
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('home.urls'))
+    path('', include('home.urls')),
+    path('products/', include('products.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
