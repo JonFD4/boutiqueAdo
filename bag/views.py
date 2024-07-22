54556e3a2492a9a707db01c602b7f141c7d18c35
Jonathan Forson <jonathanforsondanso@gmail.com>
2024-07-22 15:07:08 +0000

    Add toast

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render ,redirect
+from django.shortcuts import render, redirect, HttpResponse
+from django.urls import reverse
+from django.contrib import messages
+from products.models import Product
 
 def view_bag(request):
     """
@@ -6,10 +9,9 @@ def view_bag(request):
     """
     return render(request, 'bag/bag.html')
 
-
 def add_to_bag(request, item_id):
+    product = Product.objects.get(pk=item_id)
     # Retrieve the quantity from the POST data and convert it to an integer
-  
     quantity = int(request.POST.get('quantity', 0))
     size = None
     # Retrieve the redirect URL from the POST data
@@ -23,19 +25,19 @@ def add_to_bag(request, item_id):
     bag = request.session.get('bag', {})
 
     if size:
-        if item_id in list(bag.keys()):
+        if item_id in bag.keys():
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
-               
             else:
                 bag[item_id]['items_by_size'][size] = quantity
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        if item_id in list(bag.keys()):
+        if item_id in bag.keys():
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
             
     # Update the session with the modified bag
     request.session['bag'] = bag
@@ -43,19 +45,59 @@ def add_to_bag(request, item_id):
     # Redirect the user to the specified URL
     return redirect(redirect_url)
 
+def adjust_bag(request, item_id):
+    # Adjust the quantity from the POST data and convert it to an integer
+    quantity = int(request.POST.get('quantity', 0))
+    size = None
+    # Check if the product size is included in the POST 
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    
+    # Retrieve the current shopping bag from the session, or initialize as an empty dictionary if not present
+    bag = request.session.get('bag', {})
 
+    if size:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
 
+    # Update the session with the modified bag
+    request.session['bag'] = bag
+    
+    # Redirect the user to the specified URL
+    return redirect(reverse('view_bag'))
 
+def remove_from_bag(request, item_id):
+    try:
+        # Remove the quantity from the POST data and convert it to an integer
+        size = None
+        # Check if the product size is included in the POST 
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        
+        # Retrieve the current shopping bag from the session, or initialize as an empty dictionary if not present
+        bag = request.session.get('bag', {})
+        
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+       
+        # Update the session with the modified bag
+        request.session['bag'] = bag
+        
+        # Redirect the user to the specified URL
+        return HttpResponse(status=200)
 
-
-
-#Notes
-"""
-Session Explanation:
-Modern web applications often use sessions to store data that should persist 
-across multiple requests from the same client. 
-Sessions are typically maintained using cookies. 
-The session data can include anything that needs to be remembered between requests,
-like the contents of a shopping bag in an e-commerce application.
-
-"""
+    except Exception as e:
+        return HttpResponse(status=500)
